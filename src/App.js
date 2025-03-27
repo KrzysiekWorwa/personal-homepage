0f3dc27b4e7c1bffc32bb72a26c155ffd4a1ee74
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-27 20:52:14 +0100

    adding Container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { themeLight } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
+import { Container } from "./Components/Container";
 
 function App() {
   return (
     <ThemeProvider theme={themeLight}>
-          <GlobalStyle />
-      <>
+      <GlobalStyle />
+      <Container>
         Aplikacja
-      </>
+      </Container>
     </ThemeProvider>
   );
 }
