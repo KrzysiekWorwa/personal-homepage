20cab6908e3d8535ab5f14146fc1e44d753054fd
Krzysztof Worwa <worwak1998@gmail.com>
2025-04-24 20:29:49 +0200

    pesonalHomepageSlice

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import createSagaMiddleware from "redux-saga";
 import themeReducer from "./common/themeSlice";
+import saga from "./saga";
 
+const sagaMiddleware = createMiddleware();
 
 export const store = configureStore({
     reducer: {
