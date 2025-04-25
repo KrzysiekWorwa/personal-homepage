09a304bf30f8eed5b1c6f2bf98548713fc84b33c
Krzysztof Worwa <worwak1998@gmail.com>
2025-04-25 23:10:37 +0200

    adding styled Portfolio Tile

--- src/Components/Portfolio/Content/Repositories/styled.js
+++ src/Components/Portfolio/Content/Repositories/styled.js
@@ -13,3 +13,21 @@ export const List = styled.ul`
         grid-gap: 24px;
     }
     `;
+
+export const Tile = styled.li`
+    padding: 56px;
+    background: ${({ theme }) => theme.colors.tile.tileBackground};
+    box-shadow: ${({ theme }) => theme.colors.boxShadow};
+    border-radius: 4px;
+    border: 6px solid rgba(209, 213, 218, 0.3);
+    transition: background 0.3s;
+    transition: border-color 0.3s;
+
+    &:hover {
+        border-color: rgba(3, 102, 214, 0.2);
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        padding: 20px;
+    }
+`;
