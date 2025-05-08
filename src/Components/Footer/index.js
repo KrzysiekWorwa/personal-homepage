086ecc6b019d7537cd5c53940aa2a6f1ccca6141
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-09 00:51:11 +0200

    adding links to socialmedia

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -13,10 +13,18 @@ const Footer = () => (
                 I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to contact me 🤞
             </Info>
             <IconsWrapper>
-                <GithubIcon />
-                <FacebookIcon />
-                <LinkedinIcon />
-                <InstagramIcon />
+                <a href="https://github.com/KrzysiekWorwa" target="_blank" title="Github" rel="noreferrer">
+                    <GithubIcon />
+                </a>
+                <a href="https://www.facebook.com/krzysztof.worwa/" target="_blank" title="Facebook" rel="noreferrer">
+                    <FacebookIcon />
+                </a>
+                <a href="https://www.linkedin.com/in/krzysztof-worwa-5462b8242/" target="_blank" title="Linkedin" rel="noreferrer">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/k_worwa/" target="_blank" title="Instagram" rel="noreferrer">
+                    <InstagramIcon />
+                </a>
             </IconsWrapper>
         </Adress>
     </FooterSection>
