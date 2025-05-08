db1c4163be13a6a9c32fccccb935b9d2b1d3c9fd
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-08 22:31:10 +0200

    adding styled Loading component

---
+++ src/Components/Portfolio/Content/Loading/styled.js
@@ -0,0 +1,30 @@
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as SpinnerSvg } from "./Spinner.svg"
+
+const rotate = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+export const LoadingWrapper = styled.section`
+     margin-top: 88px;
+     text-align: center;
+     font-size: 20px;
+
+     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 32px;
+        font-size: 17px;
+    }
+`;
+
+export const LoadingSpinner = styled(SpinnerSvg)`
+    margin-top: 48px;
+    animation: ${rotate} 1s linear infinite;
+    height: auto;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 32px;
+        width: 60px;
+    }
+`;
