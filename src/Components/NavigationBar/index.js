5403b67e07055f16b5a673fc6722448520ea43e4
Krzysztof Worwa <worwak1998@gmail.com>
2025-11-06 15:31:07 +0100

    allow navbar to close after click oin link or

--- src/Components/NavigationBar/index.js
+++ src/Components/NavigationBar/index.js
@@ -1,10 +1,37 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Burger, MobileMenu, Navbar, NavItem, NavLinks, NavLogo, NavWrapper, NavIcons, ContactButton } from "./styled";
 import { Link } from "react-scroll";
 
 const NavigationBar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+    const burgerRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target) &&
+                burgerRef.current &&
+                !burgerRef.current.contains(event.target)
+            ) {
+                setIsOpen(false);
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [isOpen]);
+
+    const handleLinkClick = () => {
+        setIsOpen(false);
+    };
 
     return (
         <Navbar $open={isOpen}>
@@ -13,7 +40,7 @@ const NavigationBar = () => {
                     <NavLogo />
                 </Link>
 
-                <Burger onClick={() => setIsOpen(!isOpen)}>
+                <Burger ref={burgerRef} $open={isOpen} onClick={() => setIsOpen(!isOpen)}>
                     <div />
                     <div />
                     <div />
@@ -43,23 +70,23 @@ const NavigationBar = () => {
                     </ContactButton>
                 </NavIcons>
 
-                <MobileMenu $open={isOpen}>
-                    <NavItem to="home" smooth={true} duration={600} offset={-50}>
+                <MobileMenu ref={menuRef} $open={isOpen}>
+                    <NavItem to="home" smooth={true} duration={600} offset={-50} onClick={handleLinkClick}>
                         About me
                     </NavItem>
-                    <NavItem to="skillset" smooth={true} duration={600} offset={-350}>
+                    <NavItem to="skillset" smooth={true} duration={600} offset={-350} onClick={handleLinkClick}>
                         Skillset
                     </NavItem>
-                    <NavItem to="portfolio" smooth={true} duration={800} offset={-350}>
+                    <NavItem to="portfolio" smooth={true} duration={800} offset={-350} onClick={handleLinkClick}>
                         Portfolio
                     </NavItem>
-                    <NavItem to="3dPortfolio" smooth={true} duration={1000} offset={-340}>
+                    <NavItem to="3dPortfolio" smooth={true} duration={1000} offset={-340} onClick={handleLinkClick}>
                         3D projects
                     </NavItem>
-                    <NavItem to="tourPortfolio" smooth={true} duration={1000} offset={-340}>
+                    <NavItem to="tourPortfolio" smooth={true} duration={1000} offset={-340} onClick={handleLinkClick}>
                         Virtual Tours
                     </NavItem>
-                    <NavItem to="address" smooth={true} duration={1000} offset={-350}>
+                    <NavItem to="address" smooth={true} duration={1000} offset={-350} onClick={handleLinkClick}>
                         Contact
                     </NavItem>
                 </MobileMenu>
